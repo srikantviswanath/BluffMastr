96d6bc7b7dc9c2f81fd280007f86a9ae69f53110
srikantviswanath <srikantviswanath8@gmail.com>
2016-05-01 00:38:45 -0400

    Initial Commit

---
+++ BluffMastr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BluffMastr
+//
+//  Created by Srikant Viswanath on 5/1/16.
+//  Copyright © 2016 Srikant Viswanath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
