d21ea242d93f1d38ed089eeb6d8d6bac2458a30d
srikantviswanath <srikantviswanath8@gmail.com>
2016-07-28 19:26:16 -0400

    Added ModeAOnboardingVC: UIPageViewCOntroller for on boarding VC
    Added OverviewHelpVC and QuestionHelpVC as the first 2 screens win on
    boarding

---
+++ BluffMastr/ModeAOnBoardingPageVC.swift
@@ -0,0 +1,74 @@
+//
+//  ModeAOnBoardingViewController.swift
+//  Bluffathon
+//
+//  Created by Srikant Viswanath on 7/27/16.
+//  Copyright © 2016 Srikant Viswanath. All rights reserved.
+//
+
+import UIKit
+
+class ModeAOnboardingPageVC: UIPageViewController, UIPageViewControllerDataSource {
+
+    lazy var orderedVCs: [UIViewController] = {
+        return [self.newOnboardingVC("OverviewOnboarding"),
+                self.newOnboardingVC("QuestionVCOnboarding"),
+                self.newOnboardingVC("OvervieHelpVC")]
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        dataSource = self
+        
+        if let onboardingHelpVC = orderedVCs.first {
+            setViewControllers([onboardingHelpVC], direction: .Forward, animated: true, completion: nil)
+        }
+    }
+    
+    func newOnboardingVC(storyboardId: String) -> UIViewController {
+        return UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier(storyboardId)
+    }
+    
+    //MARK: UIPageVC DataSource Delegate Methods
+    
+    func presentationCountForPageViewController(pageViewController: UIPageViewController) -> Int {
+        return orderedVCs.count
+    }
+    
+    func presentationIndexForPageViewController(pageViewController: UIPageViewController) -> Int {
+        guard let firstViewController = orderedVCs.first, firstViewControllerIndex = orderedVCs.indexOf(firstViewController) else {
+                return 0
+        }
+        
+        return firstViewControllerIndex
+    }
+
+    
+    func pageViewController(pageViewController: UIPageViewController, viewControllerBeforeViewController viewController: UIViewController) -> UIViewController? {
+        guard let vcIndex = orderedVCs.indexOf(viewController) else {
+            return nil
+        }
+        
+        let prevIndex = vcIndex - 1
+        if (prevIndex >= 0 && orderedVCs.count > prevIndex) {
+            return orderedVCs[prevIndex]
+        } else {
+            return nil
+        }
+    }
+    
+    func pageViewController(pageViewController: UIPageViewController, viewControllerAfterViewController viewController: UIViewController) -> UIViewController? {
+        guard let vcIndex = orderedVCs.indexOf(viewController) else {
+            return nil
+        }
+        
+        let nextVCIndex = vcIndex + 1
+        if nextVCIndex < orderedVCs.count {
+            return orderedVCs[nextVCIndex]
+        } else {
+            return nil
+        }
+    }
+    
+    
+}
